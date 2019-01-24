6c766aa10f2085e9f326c70af384730c4e0b93be
JoaoNSE <nobreenrique@hotmail.com>
2019-01-24 08:56:17 -0300

    Cena de logo
    Foi colocada a Logo da PG no jogo

---
+++ Prelude/Assets/Logo/Logo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logo : MonoBehaviour {
+    
+    private Fade fade;
+	// Use this for initialization
+	void Start () {
+        
+        fade = GetComponent<Fade>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fade.done) {
+            SceneManager.LoadScene("SampleScene");
+        }
+	}
+}
