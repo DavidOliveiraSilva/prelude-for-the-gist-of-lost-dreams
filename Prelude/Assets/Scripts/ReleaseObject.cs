92a48da178a5d72b8b8eeecc8f619c27cfd4b82d
David Oliveira <clear844@gmail.com>
2019-02-19 21:26:36 -0300

    Flor Roxa e petalas da morte

---
+++ Prelude/Assets/Scripts/ReleaseObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseObject : MonoBehaviour {
+    public float duration;
+    public GameObject release;
+    public bool dead;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Death() {
+        dead = true;
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!dead) {
+            duration -= Time.deltaTime;
+            if (duration <= 0) {
+                GameObject rl = Instantiate(release);
+                rl.transform.position = transform.position;
+                Death();
+            }
+        }
+	}
+}
