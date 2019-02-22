9bff6beaa2d47fae44320e16f2d62b0ca837417c
JoaoNSE <nobreenrique@hotmail.com>
2019-02-22 18:09:25 -0300

    Poeirinha quando a porta abre

--- Prelude/Assets/Scripts/Door.cs
+++ Prelude/Assets/Scripts/Door.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 
     private Animator animator;
+    public ParticleSystem[] particles;
 
 	void Start () {
         animator = GetComponent<Animator>();
+        particlesEmission(false);
     }
 	
 	// Update is called once per frame
@@ -16,5 +18,20 @@ public class Door : MonoBehaviour {
 	}
     public void Open() {
         animator.SetBool("open", true);
+        particlesEmission(true);
+    }
+
+    public void particlesEmission(bool emision) {
+        foreach (var p in particles)
+        {
+            p.enableEmission = emision;
+        }
+    }
+
+    public void particlesStop() {
+        foreach (var p in particles)
+        {
+            p.enableEmission = false;
+        }
     }
 }
