26622294cc249b415743b4ff91011fb2d5df23a5
David Oliveira <clear844@gmail.com>
2019-04-04 17:26:41 -0300

    teste de morte da Tuane

--- Prelude/Assets/Scripts/Tuane/Player.cs
+++ Prelude/Assets/Scripts/Tuane/Player.cs
@@ -26,82 +26,84 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-        float angle = 0;
-        if (hasControl) {
-            if (Mathf.Abs(hor) > 0 || Mathf.Abs(ver) > 0) {
-                angle = Mathf.Atan2(ver, hor);
-                rb.velocity = new Vector2(speed * Time.deltaTime * Mathf.Cos(angle) * Mathf.Abs(hor), speed * Time.deltaTime * Mathf.Sin(angle) * Mathf.Abs(ver));
-            } else {
-                rb.velocity = new Vector2(0, 0);
+        if (!dead) {
+            float hor = Input.GetAxis("Horizontal");
+            float ver = Input.GetAxis("Vertical");
+            float angle = 0;
+            if (hasControl) {
+                if (Mathf.Abs(hor) > 0 || Mathf.Abs(ver) > 0) {
+                    angle = Mathf.Atan2(ver, hor);
+                    rb.velocity = new Vector2(speed * Time.deltaTime * Mathf.Cos(angle) * Mathf.Abs(hor), speed * Time.deltaTime * Mathf.Sin(angle) * Mathf.Abs(ver));
+                } else {
+                    rb.velocity = new Vector2(0, 0);
+                }
+            }
+            if (hor > 0) {
+                sr.flipX = true;
+            }
+            if (hor < 0) {
+                sr.flipX = false;
+            }
+
+            if (hasControl) {
+                if (dashLoad > 0 && Input.GetButtonDown("Dash")) {
+                    dashLoad--;
+                    Dash();
+                }
+            }
+
+            lifeTime -= Time.deltaTime;
+            if (lifeTime <= 0) {
+                startDeath();
+            }
+
+            if (dashing > 0) {
+                dashing -= Time.deltaTime;
+                rb.velocity = new Vector2(dashSpeed * Time.deltaTime * speed * Mathf.Cos(dashAngle), dashSpeed * Time.deltaTime * speed * Mathf.Sin(dashAngle));
+                if (dashing <= 0) {
+                    dashing = 0;
+                    hasControl = true;
+                    rb.velocity = new Vector2(0, 0);
+                }
             }
         }
-        if(hor > 0) {
-            sr.flipX = true;
-        }
-        if(hor < 0) {
-            sr.flipX = false;
-        }
-
-        if (hasControl) {
-            if (dashLoad > 0 && Input.GetButtonDown("Dash")) {
-                dashLoad--;
-                Dash();
-            }
-        }
-
-        lifeTime -= Time.deltaTime;
-        if(lifeTime <= 0) {
-            startDeath();
-        }
-
-        if(dashing > 0) {
-            dashing -= Time.deltaTime;
-            rb.velocity = new Vector2(dashSpeed*Time.deltaTime*speed*Mathf.Cos(dashAngle), dashSpeed * Time.deltaTime * speed * Mathf.Sin(dashAngle));
-            if (dashing <= 0) {
-                dashing = 0;
-                hasControl = true;
-                rb.velocity = new Vector2(0, 0);
-            }
-        }
-
         
         
     }
 
     public void FixedUpdate() {
         //CHECANDO COLISÃO COM CAIXA E PAREDE (QUANDO A TUANE É IMPRENSADA PELA CAIXA E PAREDE)
-        int layermask = 1 << 10;
-        layermask = ~layermask;
-        RaycastHit2D rcLeft = Physics2D.Linecast(transform.position, new Vector3(transform.position.x - 0.5f, transform.position.y, 0), layermask);
-        RaycastHit2D rcRight = Physics2D.Linecast(transform.position, new Vector3(transform.position.x + 0.5f, transform.position.y, 0), layermask);
-        RaycastHit2D rcUp = Physics2D.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y + 0.5f, 0), layermask);
-        RaycastHit2D rcDown = Physics2D.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y - 0.5f, 0), layermask);
+        if (!dead) {
+            int layermask = 1 << 10;
+            layermask = ~layermask;
+            RaycastHit2D rcLeft = Physics2D.Linecast(transform.position, new Vector3(transform.position.x - 0.5f, transform.position.y, 0), layermask);
+            RaycastHit2D rcRight = Physics2D.Linecast(transform.position, new Vector3(transform.position.x + 0.5f, transform.position.y, 0), layermask);
+            RaycastHit2D rcUp = Physics2D.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y + 0.5f, 0), layermask);
+            RaycastHit2D rcDown = Physics2D.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y - 0.5f, 0), layermask);
 
-        if (rcLeft && rcRight) {
-            if (rcLeft.transform != null && rcRight.transform != null) {
-                if ((rcLeft.transform.gameObject.tag == "Moving Box" && rcRight.transform.gameObject.tag == "Wall") ||
-                        (rcLeft.transform.gameObject.tag == "Wall" && rcRight.transform.gameObject.tag == "Moving Box") ||
-                            (rcLeft.transform.gameObject.tag == "Moving Box" && rcRight.transform.gameObject.tag == "Moving Box")) {
-                    startDeath();
+            if (rcLeft && rcRight) {
+                if (rcLeft.transform != null && rcRight.transform != null) {
+                    if ((rcLeft.transform.gameObject.tag == "Moving Box" && rcRight.transform.gameObject.tag == "Wall") ||
+                            (rcLeft.transform.gameObject.tag == "Wall" && rcRight.transform.gameObject.tag == "Moving Box") ||
+                                (rcLeft.transform.gameObject.tag == "Moving Box" && rcRight.transform.gameObject.tag == "Moving Box")) {
+                        startDeath();
+                    }
+                }
+            }
+            if (rcUp && rcDown) {
+                if (rcUp.transform != null && rcDown.transform != null) {
+                    if ((rcUp.transform.gameObject.tag == "Moving Box" && rcDown.transform.gameObject.tag == "Wall") ||
+                            (rcUp.transform.gameObject.tag == "Wall" && rcDown.transform.gameObject.tag == "Moving Box") ||
+                                (rcUp.transform.gameObject.tag == "Moving Box" && rcDown.transform.gameObject.tag == "Moving Box")) {
+                        startDeath();
+                    }
                 }
             }
         }
-        if (rcUp && rcDown) {
-            if (rcUp.transform != null && rcDown.transform != null) {
-                if ((rcUp.transform.gameObject.tag == "Moving Box" && rcDown.transform.gameObject.tag == "Wall") ||
-                        (rcUp.transform.gameObject.tag == "Wall" && rcDown.transform.gameObject.tag == "Moving Box") ||
-                            (rcUp.transform.gameObject.tag == "Moving Box" && rcDown.transform.gameObject.tag == "Moving Box")) {
-                    startDeath();
-                }
-            }
-        }
-        Debug.DrawLine(transform.position, new Vector3(transform.position.x - 0.5f, transform.position.y, 0));
-        Debug.DrawLine(transform.position, new Vector3(transform.position.x + 0.5f, transform.position.y, 0));
-        Debug.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y + 0.5f, 0));
-        Debug.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - 0.5f, 0));
+        //Debug.DrawLine(transform.position, new Vector3(transform.position.x - 0.5f, transform.position.y, 0));
+        //Debug.DrawLine(transform.position, new Vector3(transform.position.x + 0.5f, transform.position.y, 0));
+        //Debug.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y + 0.5f, 0));
+        //Debug.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - 0.5f, 0));
     }
     void CheckRC(RaycastHit2D rc) {
         if(rc.transform != null) {
@@ -133,19 +135,23 @@ public class Player : MonoBehaviour {
     
 
     public void startDeath() {
-        dead = true;
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        if (!dead) {
+            dead = true;
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.tag.Equals("Thorn")) {
-            startDeath();
-        }
-        if (col.gameObject.tag.Equals("Mortal Cell")) {
-            startDeath();
-        }
-        if (col.gameObject.tag.Equals("Purple Flower")) {
-            startDeath();
+        if (!dead) {
+            if (col.gameObject.tag.Equals("Thorn")) {
+                startDeath();
+            }
+            if (col.gameObject.tag.Equals("Mortal Cell")) {
+                startDeath();
+            }
+            if (col.gameObject.tag.Equals("Purple Flower")) {
+                startDeath();
+            }
         }
     }
 }
