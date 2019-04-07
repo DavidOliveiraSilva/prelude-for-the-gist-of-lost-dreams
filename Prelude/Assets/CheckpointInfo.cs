64012e395349139f367e9083516ed2790e583125
David Oliveira <clear844@gmail.com>
2019-04-07 04:00:24 -0300

    checkpoint

---
+++ Prelude/Assets/CheckpointInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointInfo : MonoBehaviour {
+
+    public static CheckpointInfo instance;
+    public Vector2 startposition;
+    public Vector2 lastPoint;
+    public bool ativado;
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+        if (instance != null) {
+            if (instance != this) {
+                Destroy(this.gameObject);
+            }
+        } else {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
